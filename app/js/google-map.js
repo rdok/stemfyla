df5f595fa33350439d16b91b732d77a12d9f8dc5
Rizart Dokollari <r.dokollari@gmail.com>
2014-12-04 02:50:31 +0200

    Improve navbar & prepare google maps.

---
+++ app/js/google-map.js
@@ -0,0 +1,8 @@
+/**
+ * @author Rizart Dokollari
+ * @version 12/4/2014
+ */
+
+jQuery(function ($) {
+
+});
--- app/js/main.js
+++ app/js/main.js
@@ -4,12 +4,58 @@
  */
 
 (function () {
-    var app = angular.module('stemfylaApp', []);
+    var app = angular.module('stemfylaApp', ['uiGmapgoogle-maps'])
+        .config(function (uiGmapGoogleMapApiProvider) {
+            uiGmapGoogleMapApiProvider.configure({
+                //    key: 'your api key',
+                v: '3.17',
+                libraries: 'weather,geometry,visualization'
+            });
+        })
+        .controller("mainCtrl", function ($scope, uiGmapGoogleMapApi) {
+            // Do stuff with your $scope.
+            // Note: Some of the directives require at least something to be defined originally!
+            // e.g. $scope.markers = []
+
+            // uiGmapGoogleMapApi is a promise.
+            // The "then" callback function provides the google.maps object.
+            uiGmapGoogleMapApi.then(function (maps) {
+                $scope.map = {center: {latitude: 45, longitude: -73}, zoom: 8};
+                $scope.options = {scrollwheel: false};
+            });
+        });
+
+    app.controller('AppController', function () {
+
+    });
+
+    app.controller('PanelController', function () {
+        this.tab = 1;
+
+        this.selectTab = function (tab) {
+            if (tab === null || typeof tab === 'undefined') {
+                this.tab = 1;
+            } else {
+                this.tab = tab;
+            }
+        };
+
+        this.isSelected = function (tab) {
+            return this.tab === tab;
+        };
+    });
 
     app.directive('appNavBar', function () {
         return {
             restrict: 'E',
             templateUrl: 'app/nav-bar.html'
+        };
+    });
+
+    app.directive('appContactPage', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'app/contact.html'
         };
     });
 
@@ -19,4 +65,6 @@
             templateUrl: 'app/footer.html'
         };
     });
+
+
 })();
